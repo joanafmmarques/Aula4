cb721961f04cd1b2d01304ef5eb66d8480f048bc
Joana Marques <joana.pipaa@gmail.com>
2018-03-08 17:28:35 +0000

    Add XML documentation

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -2,20 +2,34 @@
 
 namespace MyAula04
 {
+   /// <summary>
+   /// Class Aula4
+   /// </summary>
     class MyClass
     {
+        /// <summary>
+        /// Method that prints on the screen numbers from 1 to 10
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
         static void Main(string[] args)
         {
             
             CountTo10();
             
         }
-
+        /// <summary>
+        /// Method that uses CountToN() to count to 10
+        /// </summary>
         static void CountTo10()
         {
                 CountToN(10);
         }
 
+        /// <summary>
+        /// Method that counts to n
+        /// </summary>
+        /// <param name="n">Var int that will make the cicle for print numbers
+        /// from 1 to n</param>
         static void CountToN(int n)
         {
             for (int i = 1; i <= n; i++)
