17750a2fba9a01c1d3bfb9753bb2867dc253c583
Joana Marques <joana.pipaa@gmail.com>
2018-03-08 16:59:31 +0000

    Add Method CountToN()

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -8,12 +8,23 @@ namespace MyAula04
         {
             CountTo10();
             CountTo10();
+
+            CountToN(15);
+            CountToN(5);
             
         }
 
         static void CountTo10()
         {
             for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void CountToN(int n)
+        {
+            for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine(i);
             }
